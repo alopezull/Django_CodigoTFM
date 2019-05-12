1e5b9350def3fc4cdfaa62e4574bbbf53237b18d
alopezull <48690426+alopezull@users.noreply.github.com>
2019-05-12 12:30:11 +0200

    upgrade

--- upload/views.py
+++ upload/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from upload.models import AlarmsData
+from django.shortcuts import get_object_or_404, render_to_response
+
 
 # Create your views here.
 def upload_file(request):
 	respuesta = AlarmsData.objects.filter(id=2)
-	return HttpResponse(respuesta)
+	return render_to_response('upload/index.html', {'respuesta':respuesta})
 
 
